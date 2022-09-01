8974c362f338eb8efb7beb18619e254527f0b222
ssb <dhsb123@naver.com>
2022-09-01 16:27:11 +0900

    프로젝트 세팅 업로드

---
+++ src/main/java/com/greenart/news_media/NewsMediaApplication.java
@@ -0,0 +1,13 @@
+package com.greenart.news_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsMediaApplication.class, args);
+	}
+
+}
